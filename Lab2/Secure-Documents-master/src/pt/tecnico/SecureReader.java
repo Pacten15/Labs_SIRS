309ae98cacf721bcee5858fef578a835be5a89ff
Pacten15 <luismarques718@hotmail.com>
2023-11-22 19:54:38 -0500

    4.1 and 4.2 not done the rest looks fine

--- Lab2/Secure-Documents-master/src/pt/tecnico/SecureReader.java
+++ Lab2/Secure-Documents-master/src/pt/tecnico/SecureReader.java
@@ -4,11 +4,17 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.nio.file.*;
 
 public class SecureReader {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+
+        long MAX_AGE = 1000 * 6; // 1 day
+
         // Check arguments
         if (args.length < 1) {
             System.err.println("Argument(s) missing!");
@@ -16,6 +22,9 @@ public class SecureReader {
             return;
         }
         final String filename = args[0];
+
+        // Load the public key
+        PublicKey publicKey = readPublicKey(args[1]);
 
         // Read JSON object from file, and print its contets
         try (FileReader fileReader = new FileReader(filename)) {
@@ -37,8 +46,63 @@ public class SecureReader {
                     System.out.println(); // Print a newline after the final tag
                 }
             }
-
+            System.out.println("Title: " + headerObject.get("title").getAsString());
             System.out.println("Document body: " + rootJson.get("body").getAsString());
+            System.out.println("Document status: " + rootJson.get("status").getAsString());
+            
+             // Extract the signature from the JSON object
+            String signatureString = rootJson.get("signature").getAsString();
+            byte[] signatureBytes = Base64.getDecoder().decode(signatureString);
+
+            // Extract the timestamp from the JSON object
+            long timestamp = rootJson.get("timestamp").getAsLong();
+
+            
+            // Remove the signature from the JSON object
+            rootJson.remove("signature");
+
+            // Remove the timestamp from the JSON object
+            rootJson.remove("timestamp");
+
+            // Convert the JSON object to a byte array
+            String jsonString = rootJson.toString();
+            byte[] documentBytes = jsonString.getBytes();
+
+            // Verify the digital signature
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initVerify(publicKey);
+            signature.update(documentBytes);
+            boolean isVerified = signature.verify(signatureBytes);
+
+            if (isVerified) {
+                System.out.println("The document is verified.");
+            } else {
+                System.out.println("The document is not verified.");
+            }
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - timestamp > MAX_AGE) {
+                System.out.println("The document is not fresh.");
+            } else {
+                System.out.println("The document is fresh.");
+}
+
         }
+    }
+
+    private static byte[] readFile(String path) throws FileNotFoundException, IOException {
+        FileInputStream fis = new FileInputStream(path);
+        byte[] content = new byte[fis.available()];
+        fis.read(content);
+        fis.close();
+        return content;
+    }
+
+    public static PublicKey readPublicKey(String publicKeyPath) throws Exception {
+        System.out.println("Reading public key from file " + publicKeyPath + " ...");
+        byte[] pubEncoded = readFile(publicKeyPath);
+        X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(pubEncoded);
+        KeyFactory keyFacPub = KeyFactory.getInstance("RSA");
+        PublicKey pub = keyFacPub.generatePublic(pubSpec);
+        return pub;
     }
 }
--- Lab2/Secure-Documents-master/src/pt/tecnico/SecureWriter.java
+++ Lab2/Secure-Documents-master/src/pt/tecnico/SecureWriter.java
@@ -1,7 +1,11 @@
 package pt.tecnico;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+import java.nio.file.*;
+
 
 import com.google.gson.*;
 
@@ -9,7 +13,7 @@ import com.google.gson.*;
  * Example of JSON writer.
  */
 public class SecureWriter {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         // Check arguments
         if (args.length < 1) {
             System.err.println("Argument(s) missing!");
@@ -18,11 +22,15 @@ public class SecureWriter {
         }
         final String filename = args[0];
 
+        // Load the private key
+        PrivateKey privateKey = readPrivateKey(args[1]);
+
+
         // Create bank statement JSON object
         JsonObject jsonObject = new JsonObject();
 
         JsonObject headerObject = new JsonObject();
-        headerObject.addProperty("author", "Ultron");
+        headerObject.addProperty("author", "Ultrn");
         headerObject.addProperty("version", 2);
         headerObject.addProperty("title", "Age of Ultron");
         JsonArray tagsArray = new JsonArray();
@@ -32,13 +40,46 @@ public class SecureWriter {
         jsonObject.add("header", headerObject);
 
         jsonObject.addProperty("body", "I had strings but now I'm free");
+        jsonObject.addProperty("status", "published");
+
+         // Convert the JSON object to a byte array
+         String jsonString = jsonObject.toString();
+         byte[] documentBytes = jsonString.getBytes();
+
+        // Generate the digital signature
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(privateKey);
+        signature.update(documentBytes);
+        byte[] signatureBytes = signature.sign();
+
+        // Add the signature to the JSON object
+        jsonObject.addProperty("signature", Base64.getEncoder().encodeToString(signatureBytes));
+
+        // Add the current timestamp as a freshness token
+        jsonObject.addProperty("timestamp", System.currentTimeMillis());
 
         // Write JSON object to file
         try (FileWriter fileWriter = new FileWriter(filename)) {
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             gson.toJson(jsonObject, fileWriter);
         }
+    }
 
-        
+
+    private static byte[] readFile(String path) throws FileNotFoundException, IOException {
+        FileInputStream fis = new FileInputStream(path);
+        byte[] content = new byte[fis.available()];
+        fis.read(content);
+        fis.close();
+        return content;
+    }
+
+
+    public static PrivateKey readPrivateKey(String privateKeyPath) throws Exception {
+        byte[] privEncoded = readFile(privateKeyPath);
+        PKCS8EncodedKeySpec privSpec = new PKCS8EncodedKeySpec(privEncoded);
+        KeyFactory keyFacPriv = KeyFactory.getInstance("RSA");
+        PrivateKey priv = keyFacPriv.generatePrivate(privSpec);
+        return priv;
     }
 }
